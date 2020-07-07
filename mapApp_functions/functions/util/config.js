08b360a9e8555439d651c1d60a866e22aa3b7c7c
mariksep <47556698+mariksep@users.noreply.github.com>
2020-07-07 14:23:22 +0300

    Delete config.js

--- mapApp_functions/functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyDNJcwrTR8MAgQ3Y3H4FLcYDTLrKNnCU0w",
-  authDomain: "mapapp-b8e8c.firebaseapp.com",
-  databaseURL: "https://mapapp-b8e8c.firebaseio.com",
-  projectId: "mapapp-b8e8c",
-  storageBucket: "mapapp-b8e8c.appspot.com",
-  messagingSenderId: "615655981456",
-  appId: "1:615655981456:web:4b8cb4dbab9ee546f8b315",
-};
